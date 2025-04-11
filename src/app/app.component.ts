c4cd8bc44c8b8fd9fc3beecd0a8338661f1e0371
raymond.chandra <raymond.chandra@fdmgroup.com>
2025-04-11 13:09:35 -0400

    Adding assets, Header, and Home Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'my-angular-app';
-}
+
+export class AppComponent {}
