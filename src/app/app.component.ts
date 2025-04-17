e13a93ace8359d00a081a869b16a1b8af072c441
raymond.chandra <raymond.chandra@fdmgroup.com>
2025-04-17 18:08:17 -0400

    Adding signal, and inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+
+/**
+ * NOTE
+ * selector -> what will be the html tag of this component
+ * imports -> which child components we can use for this. It will be in Array form
+ * templateUrl -> html link
+ * styleUrl -> css link
+ */
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    HomeComponent,
     HeaderComponent,
     UserComponent
 ],
@@ -16,4 +21,23 @@ import { UserComponent } from "./user/user.component";
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  
+  /** 
+   * NOTE
+   * Above code is a property that's why you don't need the let OR const keyword
+   * This is like a constructor actually in Typescript
+   * 
+   * However, Typescript allows you to write it like that
+   * If we elaborate, it is like this actually:
+   * 
+   *  users: any; // Declare the property
+
+      constructor() {
+        this.users = DUMMY_USERS; // Assign it inside the constructor
+      }
+   * 
+   */
+
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, signal, computed, Input } from '@angular/core';
 
+// -- Just for 1 user --
+/**
 // Creating a constant for a randomIndex
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+export class UserComponent {
+  // Getting a random user
+  // -- Option A - using regular method, and zone.js for zoning change detection
+  // selectedUser = DUMMY_USERS[randomIndex];
+  
+  // -- Option B - using signal (from Angular v16)
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  
+  // -- Option A - using regular method, and zone.js for zoning change detection
+  // Create a getter
+  // We can call this just like a property that means you don't need ()
+  //get imagePath() {
+  //  return 'assets/users/' + this.selectedUser.avatar;
+  //}
+  
+  // -- Option B - using compute in the signal
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  
+
+  // Trigger when you click
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    
+    // -- Option A - using regular method, and zone.js for zoning change detection
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+
+    // -- Option B - using signal
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
+*/
 
 @Component({
   selector: 'app-user',
@@ -13,17 +46,23 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 export class UserComponent {
-  // Getting a random user
-  selectedUser = DUMMY_USERS[randomIndex];
+  // Getting the input from the parent component - App
+  /**
+   * NOTE
+   * required -> true -> means that this property is required, otherwise IDE will throw an error
+   */
+  @Input({
+    required: true
+  }) avatar !: string;
 
-  // Create a getter
-  // We can call this just like a property that means you don't need ()
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
-
-  // Trigger when you click
+  @Input({
+    required: true
+  }) name !: string;
+  
+  // Use signal to change the imagePath
+  imagePath = computed(() => 'assets/users/' + this.avatar)
+  
   onSelectUser() {
-    alert("clicked!");
+    
   }
 }
